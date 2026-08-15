47fe4b10b3924675d2a5ee4c71969f4eb0e566de
te-sa <teresa_saller@freenet.de>
2026-08-15 12:44:05 +0200

    Add sitemap and robots.txt

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,14 @@
+import type { APIRoute } from "astro";
+
+// https://docs.astro.build/en/guides/integrations-guide/sitemap/
+const getRobotsTxt = (sitemapURL: URL) => `\
+User-agent: *
+Allow: /
+
+Sitemap: ${sitemapURL.href}
+`;
+
+export const GET: APIRoute = ({ site }) => {
+	const sitemapURL = new URL("sitemap-index.xml", site);
+	return new Response(getRobotsTxt(sitemapURL));
+};
