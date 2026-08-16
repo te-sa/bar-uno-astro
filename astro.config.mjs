e5d78b0c964bd130274339d0b1f87024d095097a
te-sa <teresa_saller@freenet.de>
2026-08-16 19:30:56 +0200

    SEO improvements
    - add trailingSlash always
    - add structured data
    - lower image quality

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
 	site: "https://www.bar-uno.de",
+	trailingSlash: "always",
 
 	vite: {
 		plugins: [tailwindcss()],
--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -4,6 +4,7 @@ import type { APIRoute } from "astro";
 const getRobotsTxt = (sitemapURL: URL) => `\
 User-agent: *
 Allow: /
+Disallow: /admin/
 
 Sitemap: ${sitemapURL.href}
 `;
